8eb659eb1f926fb93d2823fc05e4cc0208585c87
mantran55 <145571137+mantran55@users.noreply.github.com>
2025-09-03 20:42:45 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxOs9lOhOX1NGnrztK5RnaSlGJ6F0ykVnDIbQJUUs_fL6AibJeNt88De32e8WTmUeM4ZQ/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbx09PpwSpt9q4mv2Ml6_9pVR8HKzL8SuoT8_H6y6CDZhw1-jHMM0fp9o13tAvGnLTWH/exec";
 
 // Hàm gọi API sử dụng JSONP
 function callApiJsonp(params) {
@@ -299,4 +299,5 @@ window.updateWorkingHours = updateWorkingHours;
 window.publishSchedule = publishSchedule;
 window.getEmployees = getEmployees;
 window.showNotification = showNotification;
+
 
