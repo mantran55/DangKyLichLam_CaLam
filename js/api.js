699d0f617c37d22796b192cee72ba298f9b3a391
mantran55 <145571137+mantran55@users.noreply.github.com>
2025-09-03 16:53:50 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbwqjbIwrt7odmqcE18zptgh1mI0uTqywMGhLMuv2rfHLvqypU7HYw6_9yAXOfgNtRmpDw/exec"; // Thay bằng URL của bạn
+const API_URL = "https://script.google.com/macros/s/AKfycbx9y5BCGVaUeEDWTgKJiKBzJKayLxVrksVdO4MY2IgfZSlcrV9mLlBc9JHn-LYLj7BN/exec"; // Thay bằng URL của bạn
 
 async function callApi(params) {
   try {
@@ -215,5 +215,6 @@ window.updateWorkingHours = updateWorkingHours;
 window.publishSchedule = publishSchedule;
 window.getEmployees = getEmployees;
 window.showNotification = showNotification;
+
 
 
